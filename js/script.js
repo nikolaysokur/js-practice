bba26d3412b58a14a5a3bd0f39db29eca50f8c76
Nick <nikolaysokur@gmail.com>
2022-03-24 16:25:33 +0200

    create first program

--- js/script.js
+++ js/script.js
@@ -1,5 +1,21 @@
 "use strict";
 
-const user = "Ivan";
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-alert(`Привет, ${user}`);
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false
+};
+
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
